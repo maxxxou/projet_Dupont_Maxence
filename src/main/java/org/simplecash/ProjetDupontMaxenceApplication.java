9609b1d588e2b04a494550fc890eb87f3f8e43e2
maxence.dupont <91914871+maxxxou@users.noreply.github.com>
2025-11-28 09:45:48 +0100

    Initial commit

---
+++ src/main/java/org/simplecash/ProjetDupontMaxenceApplication.java
@@ -0,0 +1,13 @@
+package org.simplecash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDupontMaxenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDupontMaxenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/simplecash/ProjetDupontMaxenceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simplecash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDupontMaxenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
