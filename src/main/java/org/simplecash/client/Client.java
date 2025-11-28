32accad9992696b8286210b3e08063b84f2a4b8e
maxence.dupont <91914871+maxxxou@users.noreply.github.com>
2025-11-28 10:18:50 +0100

    feat: switch to lombok in client setters

--- src/main/java/org/simplecash/client/Client.java
+++ src/main/java/org/simplecash/client/Client.java
@@ -5,7 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @Entity
 @Table(name = "clients")
 public class Client {
@@ -14,11 +17,17 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String lastName;
+    @Setter
     private String firstName;
+    @Setter
     private String address;
+    @Setter
     private String postalCode;
+    @Setter
     private String city;
+    @Setter
     private String phone;
 
     protected Client() {
@@ -33,55 +42,4 @@ public class Client {
         this.phone = phone;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
